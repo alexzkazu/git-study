86cb153b46db66c80e87fd4105bc6298bf0f3338
alexzkazu <alex.ziebol@gmail.com>
2014-06-18 00:45:11 -0700

    got mocha/chai workin woot

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,36 @@
-module.exports = function(grunt) {
- 
+// Gruntfile.js
+module.exports = function(grunt){
   grunt.initConfig({
-    jshint: {
-      all: ['Gruntfile.js', 'hello.js']
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Mocha
+    mocha: {
+      all: {
+        src: ['tests/testrunner.html'],
+      },
+      options: {
+        run: true
+      }
     }
   });
- 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
- 
+
+  // Load grunt mocha task
+  grunt.loadNpmTasks('grunt-mocha');
+
+  grunt.registerTask('default', ['mocha']);
 };
+
+
+// // Old Test
+// module.exports = function(grunt) {
+ 
+//   grunt.initConfig({
+//     jshint: {
+//       all: ['Gruntfile.js', 'hello.js']
+//     }
+//   });
+ 
+//   grunt.loadNpmTasks('grunt-contrib-jshint');
+//   grunt.registerTask('default', 'jshint');
+ 
+// };
